ed664ce4a2cc0a7d7cd231596782f83a30159654
kiran tanange <kirantanange@gmail.com>
2023-07-13 19:18:53 +0530

    mutidimensional array

---
+++ src/array/multidimentionalArray/Department.java
@@ -0,0 +1,38 @@
+package array.multidimentionalArray;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {	
+	
+	
+	public static void main(String args[]){
+		List<String[]> emp=new ArrayList();
+		String[] employee1=new String[]{"vishal","hase","rajapur"};
+		String[] employee2=new String[]{"kiran","tanange","miraj"};
+		String[] employee3=new String[]{"irfan","momin","Pune"};	
+					
+		emp.add(employee1);
+		emp.add(employee2);
+		emp.add(employee3);
+		
+		 String[] a=emp.get(0);
+		 String ans=a[0];
+		
+		System.out.println(ans);
+		
+		String[][] multiArray=new String[3][3];
+		
+		for(int i=0;i<multiArray.length;i++){
+			for(int j=0;j<multiArray[i].length;j++){
+				multiArray[i][j]=emp.get(i)[j];				
+			}
+		}
+		for(int i=0;i<multiArray.length;i++){
+			for(int j=0;j<multiArray[i].length;j++){			
+		
+		System.out.println(multiArray[i][j]);}}
+	}
+
+}
